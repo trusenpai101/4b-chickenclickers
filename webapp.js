2eb66709afe996c3e80218c08c9876cf158dccb5
Alicia Shi <alicia.shi@warriorlife.net>
2018-01-23 14:20:22 -0800

    chaged the javascriptgrj
    buy thingy is open thingy

--- webapp.js
+++ webapp.js
@@ -1,29 +1,63 @@
 // JavaScript Document
 
+// global variables
 var eggs = 0;
+var chicks = 0;
+var cartons = 0;
+var farms = 0;
+
+// buy("chicks");
+function buy(sel) {
+	switch(sel) {
+		case "chicks":
+			if(eggs >= 10) {
+				eggs-=10;
+				chicks++;
+			}
+			break;
+		case "cartons":
+			if(eggs >= 10) {
+				eggs-=10;
+				cartons++;
+			break;
+		case "farms":
+			if(eggs >= 10) {
+				eggs-=10;
+				farms++;
+			break;
+		case "eggtopia":
+			if(eggs >= 10) {
+				eggs-=10;
+				eggtopia++;
+			break;
+		case "universee":
+			if(eggs >= 10) {
+				eggs-=10;
+				universee++;
+			break;
+	}
+}
+
 
 function chicken_clicker() {
 	eggs++;
 	document.getElementById("eggs").value = eggs;
 }
 
-var chicks = 0;
 
 function buy_chick() {
 	chicks++;
 	document.getElementById("chicks").value = chicks;
 }
 
-var cartons = 0;
 
 function buy_carton() {
 	cartons++;
 	document.getElementById("cartons").value = cartons;
 }
 
-var farms = 0;
 
-function buy_farm() {
+function buy_farms() {
 	farms++;
 	document.getElementById("farms").value = farms;
 }
@@ -35,9 +69,9 @@ function buy_eggtopia() {
 	document.getElementById("eggtopia").value = eggtopia;
 }
 	
-var universe = 0;
+var universee = 0;
 
 function buy_universe() {
-	universe++;
-	document.getElementById("universe").value = chicken_universe;
+	universee++;
+	document.getElementById("universee").value = universee;
 }
