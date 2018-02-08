11c0cdc1a618a5566ef43c05c0bdd4b79919bc87
Alicia Shi <alicia.shi@warriorlife.net>
2018-02-08 14:29:30 -0800

    cookies work
    the page saves progress

--- webapp.js
+++ webapp.js
@@ -1,5 +1,10 @@
 // JavaScript Document
 
+//var name = prompt("Hi there! What do you want to name you egg company?");
+	//var valid=confirm("is " +name+ " correct?")
+	//document.write(" "+name+" ");
+	
+	
 // global variables
 var eggs = 0;
 var chicks = 0;
@@ -8,6 +13,12 @@ var trucks = 0;
 var farms = 0;
 var eggtopia = 0;
 var universee = 0;
+
+window.onload = function() {
+	// load cookies
+	load_cookies();
+	update_values();
+};
 
 function chicken_clicker() {
 	eggs++;
@@ -65,51 +76,33 @@ function update_values() {
 	document.getElementById("farms").value= farms;
 	document.getElementById("eggtopia").value= eggtopia;
 	document.getElementById("universee").value= universee;
+	
 }
 
 setInterval(bonus, 100);
 function bonus() {
 	eggs+=chicks*0.05;
 	eggs+=cartons*0.1;
-	eggs+=trucks*0.3;
-	eggs+=farms*0.5;
-	eggs+=eggtopia*0.8;
-	eggs+=universee*10;
+	eggs+=trucks*0.15;
+	eggs+=farms*0.25;
+	eggs+=eggtopia*0.4;
+	eggs+=universee*5;
 	update_values();
+	save_game();
 }
 
-setInterval(gold, 1000000000);
-function gold() {
-	eggs+=500;
-}
 
-function save_game() {
-	var exdays = 120;
-	var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-	document.cookie = "eggs=" + eggs + ";" + expires; // "eggs=10"
-	document.cookie = "chicks="+ chicks + ";" + expires;
-	document.cookie = "cartons="+ cartons + ";" + expires;
-	document.cookie = "trucks="+ trucks + ";" + expires;
-	document.cookie = "farms="+ farms + ";" + expires;
-	document.cookie = "eggtopia="+ eggtopia + ";" + expires;
-	document.cookie = "universee="+ universee + ";" + expires;
-	
-}
 
 function clear(){
-	var eggs = 0;
-	var chicks= 0;
-	var cartons = 0;
-	var truck = 0;
-	var farm = 0;
-	var eggtopia = 0;
-	var universee = 0;
+	eggs = 0;
+	chicks= 0;
+	cartons = 0;
+	trucks = 0;
+	farms = 0;
+	eggtopia = 0;
+	universee = 0;
 }
 
-function load_game() {
-}
 function changeCSS(cssFile, cssLinkIndex) {
 
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
@@ -122,3 +115,44 @@ function changeCSS(cssFile, cssLinkIndex) {
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
 
+// COOKIES
+function save_game() {
+	var exdays = 365;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = "eggs=" + eggs + ";" + expires;
+    document.cookie = "chicks=" + chicks + ";" + expires;
+    document.cookie = "trucks=" + trucks + ";" + expires;
+    document.cookie = "farms=" + farms + ";" + expires;
+    document.cookie = "cartons=" + cartons + ";" + expires;
+    document.cookie = "eggtopia=" + eggtopia + ";" + expires;
+    document.cookie = "universee=" + universee + ";" + expires;
+
+}
+
+function get_cookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function load_cookies() {
+    eggs = Number(get_cookie("eggs"));
+    chicks = Number(get_cookie("chicks"));
+    cartons = Number(get_cookie("cartons"));
+    trucks = Number(get_cookie("trucks"));
+    farms = Number(get_cookie("farms"));
+    eggtopia = Number(get_cookie("eggtopia"));
+    universee = Number(get_cookie("universee"));
+}
